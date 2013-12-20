dbd9f05802e04f3c0ff8bd8a7cf8005a1ef5af88
Gary Patricelli <garypatricelli@gmail.com>
2013-12-20 16:48:19 -0500

    implement idle loop with IP checking

--- ip_notify.py
+++ ip_notify.py
@@ -1,23 +1,40 @@
+import time
 import urllib2
 import sys
 from bs4 import BeautifulSoup
 
-# Web site to pull ip from 
+# Web site to pull IP from 
 pull_site = urllib2.urlopen("http://ipecho.net").read()
-ipadress = ""
+ip_adress = ""
 
-# Pulls the current IP adress from the "pull_site" and returns it as a string
+# Pulls the current IP address from the "pull_site" and returns it as a string
 def get_ip():
     # Parse the site
     soup = BeautifulSoup(pull_site)
     # Get the html as a string
     html_code = str(soup)
-    # Calculate the start of the IP adress
+    # Calculate the start of the IP address
     start_index = 15 + html_code.find("<h1>")
-    # Calculate the end of the IP adress
+    # Calculate the end of the IP address
     end_index = html_code.find("</h1>")
-    # Return the ip adress from the html
+    # Return the IP address from the html
     return html_code[start_index:end_index]
 
-  
+# Emails the specified user with the new IP address
+def notify():
+    # for testing
+    print ip_adress
+
+
+# Loop forever, checking for change occasionally
+while True:
+    current_ip_adress = get_ip()
+    if ip_adress == current_ip_adress:
+        # IP has not changed, sleep
+        time.sleep(900)
+    else:
+        # Update the IP address 
+        ip_adress = current_ip_adress
+        # Notify the user of the change
+        notify()
 
